cd9def6eb37e4f4f5888ddaf1ed196803c435926
JulianP911 <j.padilla@uniandes.edu.co>
2022-04-24 23:15:09 -0500

    Documentación de los endpoints desarrollados

--- DataModel.py
+++ DataModel.py
@@ -1,9 +1,11 @@
+# Importar las librerías necesarias para el manejo de los datos 
 from pydantic import BaseModel
 from typing import List
 
+# Clase DataModel
 class DataModel(BaseModel):
 
-# Estas varibles permiten que la librería pydantic haga el parseo entre el Json recibido y el modelo declarado.
+    # Estas varibles permiten que la librería pydantic haga el parseo entre el Json recibido y el modelo declarado
     adult_mortality: float
     infant_deaths: float
     alcohol: float
@@ -23,19 +25,24 @@ class DataModel(BaseModel):
     income_composition_of_resources	: float
     schooling: float
 
-#Esta función retorna los nombres de las columnas correspondientes con el modelo esxportado en joblib.
+    # Esta función retorna los nombres de las columnas correspondientes con el modelo exportado en joblib
     def columns():
         return ["Adult Mortality", "infant deaths", "Alcohol","percentage expenditure","Hepatitis B", "Measles", "BMI",
                 "under-five deaths", "Polio", "Total expenditure", "Diphtheria", "HIV/AIDS", "DGP", "Population",
                 "thinness 10-19 years", "thinness 5-9 years", "Income composition of resources", "Schooling"]
 
+# Clase DataModelList
 class DataModelList(BaseModel):
+
+    # Está varible permiten que la librería pydantic haga el parseo entre el Json recibido y el modelo declarado
     data: List[DataModel]
 
+# Clase DataModelPredictVariable
 class DataModelPredictVariable(BaseModel):
 
-# Estas varibles permiten que la librería pydantic haga el parseo entre el Json recibido y el modelo declarado.
+    # Estas varibles permiten que la librería pydantic haga el parseo entre el Json recibido y el modelo declarado
     life_expectancy: float
 
+    # Esta función retorna el nombre de las columna correspondiente con el modelo exportado en joblib
     def column():
         return "Life expectancy"
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Importar las librerías necesarias para ofrecer los requerimientos de negocio
 import json
 from plistlib import load
 from typing import Optional
@@ -12,35 +13,30 @@ from joblib import load
 from fastapi.encoders import jsonable_encoder
 from fastapi import FastAPI
 
+# Creación de la instancia de FastAPI
 app = FastAPI()
 
-
+# Endpoint inicial de la API
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-
+# Endpoint 1: Realizar predicciones con el modelo ML de regresión lineal
 @app.post("/predict")
 def make_predictions(data: DataModelList):
     df = convert_json_to_dataframe(data)
     df.columns = DataModel.columns()
-    print(df)
     model = load("assets/modelo.joblib")
     result = model.predict(df)
     lists = result.tolist()
     json_predict = json.dumps(lists)
     return json_predict
 
+# Endpoint 2: Determinar la métrica de mse del modelo ML de regresión lineal
 @app.post("/mse")
 def mse(data: DataModelList, dataModePredictVariable: DataModelPredictVariable):
     df = convert_json_to_dataframe(data)
     df.columns = DataModel.columns()
-    print(df)
     model = load("assets/modelo.joblib")
     result = model.predict(df)
     dict = jsonable_encoder(dataModePredictVariable)
@@ -49,6 +45,7 @@ def mse(data: DataModelList, dataModePredictVariable: DataModelPredictVariable):
     result = np.sqrt(mse)
     return {"mse": mse}
 
+# Función: Está se encarga de convertir los datos recibidos a través del JSON en un dataframe para que puedan ser usados en el modelo ML
 def convert_json_to_dataframe(data):
     dict = jsonable_encoder(data)
     dataframe = json_normalize(dict['data']) 
